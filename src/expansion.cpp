c789042c3c7b1aea5bc4a8174a2b7dbf0b36c4cc
Dimitris Panokostas <midwan@gmail.com>
2025-03-19 15:39:03 +0100

    chore: fix Latin1 character to UTF8

--- src/expansion.cpp
+++ src/expansion.cpp
@@ -5410,7 +5410,7 @@ const struct expansionromtype expansionroms[] = {
 	},
 #endif
 	{
-		_T("a1000wom512k"), _T("A1000 512k WOM"), _T("J\F6rg Huth"),
+		_T("a1000wom512k"), _T("A1000 512k WOM"), _T("Jörg Huth"),
 		NULL, NULL, NULL, NULL, ROMTYPE_512KWOM | ROMTYPE_NOT, 0, 0, BOARD_NONAUTOCONFIG_BEFORE, true,
 		NULL, 0,
 		false, EXPANSIONTYPE_INTERNAL
